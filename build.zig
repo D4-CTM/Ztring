f2f2878ae7262ad033aa9422293103505f7d847a
D4-CTM <josuedelcid325@gmail.com>
2025-05-18 12:34:16 -0600

    New testing thing

--- build.zig
+++ build.zig
@@ -4,29 +4,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-//    const lib_mod = b.createModule(.{
-//        .root_source_file = b.path("src/string.zig"),
-//        .target = target,
-//        .optimize = optimize,
-//    });
-
-    _ = b.addModule("string", .{
+    const string_mod = b.addModule("string", .{
         .root_source_file = b.path("src/string.zig"),
     });
 
-//    const lib = b.addLibrary(.{
-//        .linkage = .static,
-//        .name = "string",
-//        .root_module = lib_mod,
-//    });
+    b.modules.put("string", string_mod);
 
     const lib_test = b.addTest(.{
         .root_source_file = "src/string-tests.zig",
         .target = target,
         .optimize = optimize,
     });
-
-//    b.installArtifact(lib);
 
     const testing = b.step("tests", "Testing if the string is not broken");
     testing.dependOn(lib_test);
