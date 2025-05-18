31a62e2aa6638a16950881443036e8a57a111d0d
D4-CTM <josuedelcid325@gmail.com>
2025-05-18 14:05:08 -0600

    testing removed

--- build.zig
+++ build.zig
@@ -6,14 +6,16 @@ pub fn build(b: *std.Build) void {
 
     _ = b.addModule("string", .{
         .root_source_file = b.path("src/string.zig"),
-    });
-
-    const lib_test = b.addTest(.{
-        .root_source_file = b.path("src/string-tests.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const testing = b.step("tests", "Testing if the string is not broken");
-    testing.dependOn(lib_test);
+//    const lib_test = b.addTest(.{
+//        .root_source_file = b.path("src/string-tests.zig"),
+//        .target = target,
+//        .optimize = optimize,
+//    });
+
+//    const testing = b.step("tests", "Testing if the string is not broken");
+//    testing.dependOn(lib_test.step);
 }
