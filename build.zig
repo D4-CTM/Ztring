47c7671e2b0a10a2cd3aeda6479d72b2f3c54018
D4-CTM <josuedelcid325@gmail.com>
2025-05-18 12:09:47 -0600

    Changes to try and make it work

--- build.zig
+++ build.zig
@@ -4,21 +4,21 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/string.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+//    const lib_mod = b.createModule(.{
+//        .root_source_file = b.path("src/string.zig"),
+//        .target = target,
+//        .optimize = optimize,
+//    });
 
     _ = b.addModule("string", .{
         .root_source_file = b.path("src/string.zig"),
     });
 
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "string",
-        .root_module = lib_mod,
-    });
+//    const lib = b.addLibrary(.{
+//        .linkage = .static,
+//        .name = "string",
+//        .root_module = lib_mod,
+//    });
 
     const lib_test = b.addTest(.{
         .root_source_file = "src/string-tests.zig",
@@ -26,7 +26,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    b.installArtifact(lib);
+//    b.installArtifact(lib);
 
     const testing = b.step("tests", "Testing if the string is not broken");
     testing.dependOn(lib_test);
